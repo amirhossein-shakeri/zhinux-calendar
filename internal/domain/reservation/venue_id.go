91d585e09b8ddcafa6e0309f81a4462a7887e561
Amirhossein Shakeri <amirhossein.shakeri.work@gmail.com>
2026-06-04 10:52:58 +0330

    refactor: define venue ID errors

--- internal/domain/reservation/venue_id.go
+++ internal/domain/reservation/venue_id.go
@@ -1,7 +1,23 @@
 package reservation
 
+import "fmt"
+
+var (
+	ErrVenueIDInvalid  = fmt.Errorf("invalid venue ID")
+	ErrVenueIDZero     = fmt.Errorf("venue ID can't be zero")
+	ErrVenueIDNegative = fmt.Errorf("venue ID can't be negative")
+)
+
 type VenueID int
 
-func (id VenueID) IsValid() bool {
-	return id > 0
+func (id VenueID) Validate() error {
+	if id == 0 {
+		return fmt.Errorf("%w: %w", ErrVenueIDInvalid, ErrVenueIDZero)
+	}
+
+	if id < 0 {
+		return fmt.Errorf("%w: %w", ErrVenueIDInvalid, ErrVenueIDNegative)
+	}
+
+	return nil
 }
